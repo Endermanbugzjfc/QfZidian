a3dc05c88d281d18f1c4ea798623f2349fe91e87
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-02-22 01:15:20 +0800

    Added getAutoUpdateRepo()

--- QfZidian/src/Endermanbugzjfc/QfZidian/config/ConfigRoot.php
+++ QfZidian/src/Endermanbugzjfc/QfZidian/config/ConfigRoot.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 
 namespace Endermanbugzjfc\QfZidian\config;
 
+use function trim;
+
 class ConfigRoot
 {
 
     public string $AutoUpdateRepo = "qloog/sensitive_words";
+
+    public function getAutoUpdateRepo() : string {
+        return trim($this->AutoUpdateRepo);
+    }
 
 }
