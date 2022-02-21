33f28a3ffd4a540726c7e93ca76ae0d418a6372e
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-02-22 01:01:19 +0800

    Implemented files reading stuff in ProcessDownloadedDataTask.php

--- QfZidian/src/Endermanbugzjfc/QfZidian/update/ProcessDownloadedDataTask.php
+++ QfZidian/src/Endermanbugzjfc/QfZidian/update/ProcessDownloadedDataTask.php
@@ -6,17 +6,24 @@ namespace Endermanbugzjfc\QfZidian\update;
 
 use pocketmine\scheduler\AsyncTask;
 use ZipArchive;
+use function file_get_contents;
 use function is_callable;
+use function serialize;
+use function unserialize;
 
 class ProcessDownloadedDataTask extends AsyncTask
 {
 
+    protected string $readFilesSerialized;
+
     public function __construct(
         protected string $source,
         protected string $dest,
-        callable $callback = null
+        array            $readFiles,
+        callable         $callback = null
     )
     {
+        $this->readFilesSerialized = serialize($readFiles);
         $this->storeLocal("callback", $callback);
     }
 
@@ -26,14 +33,29 @@ class ProcessDownloadedDataTask extends AsyncTask
         $ok = $zip->open(
             $this->source
         );
-        if ($ok !== true) {
-            $this->setResult($ok);
-            return;
+        try {
+            if ($ok !== true) {
+                return;
+            }
+            $ok2 = $zip->extractTo(
+                $this->dest
+            );
+            if ($ok2 !== true) {
+                return;
+            }
+
+            $readFiles = unserialize($this->readFilesSerialized);
+            foreach ($readFiles as $file) {
+                $content = file_get_contents($file);
+                $fileResults[$file] = $content;
+            }
+        } finally {
+            $this->setResult([
+                $ok,
+                $ok2 ?? null,
+                $fileResults ?? []
+            ]);
         }
-        $ok2 = $zip->extractTo(
-            $this->dest
-        );
-        $this->setResult($ok2);
     }
 
     public function onCompletion() : void
