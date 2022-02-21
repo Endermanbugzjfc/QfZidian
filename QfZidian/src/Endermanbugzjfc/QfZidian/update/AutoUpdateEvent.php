5307a55ccd07178c480b85fff047ba8253724ff1
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-02-22 02:49:59 +0800

    Make $newSha non-null

--- QfZidian/src/Endermanbugzjfc/QfZidian/update/AutoUpdateEvent.php
+++ QfZidian/src/Endermanbugzjfc/QfZidian/update/AutoUpdateEvent.php
@@ -17,15 +17,15 @@ class AutoUpdateEvent extends PluginEvent implements Cancellable
     /**
      * Cancelling this event will stop the plugin from downloading the file and from updating the last Sha file, also from reloading dictionary content.
      * @param string $url URL to the updated dictionary content source file.
+     * @param string $newSha Will be displayed in log message.
      * @param string|null $lastShaFile File to save the new Sha which will be used in the next auto update.
-     * @param string|null $oldSha Will be displayed in log message.
-     * @param string|null $newSha Will be displayed in log message.
+     * @param string|null $oldSha Will be displayed in log message if is not null.
      */
     public function __construct(
-        protected string $url,
+        protected string  $url,
+        protected string  $newSha,
         protected ?string $lastShaFile = null,
-        protected ?string $oldSha = null,
-        protected ?string $newSha = null
+        protected ?string $oldSha = null
     )
     {
         parent::__construct(QfZidian::getInstance());
@@ -48,14 +48,6 @@ class AutoUpdateEvent extends PluginEvent implements Cancellable
     }
 
     /**
-     * @return string|null
-     */
-    public function getNewSha() : ?string
-    {
-        return $this->newSha;
-    }
-
-    /**
      * @param string $url
      */
     public function setUrl(string $url) : void
@@ -64,10 +56,18 @@ class AutoUpdateEvent extends PluginEvent implements Cancellable
     }
 
     /**
-     * > WARNING: Changing this will not update the URL itself.
-     * @param string|null $newSha
+     * @return string
      */
-    public function setNewSha(?string $newSha) : void
+    public function getNewSha() : string
+    {
+        return $this->newSha;
+    }
+
+    /**
+     * > WARNING: Changing this will not update the URL itself.
+     * @param string $newSha
+     */
+    public function setNewSha(string $newSha) : void
     {
         $this->newSha = $newSha;
     }
