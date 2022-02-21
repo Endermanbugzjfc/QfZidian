848c3f519866315ea84cd831d18a2ab5df57fdb6
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-02-22 02:18:54 +0800

    Implemented AutoUpdateEvent.php

--- QfZidian/src/Endermanbugzjfc/QfZidian/update/AutoUpdateEvent.php
+++ QfZidian/src/Endermanbugzjfc/QfZidian/update/AutoUpdateEvent.php
@@ -14,9 +14,54 @@ class AutoUpdateEvent extends PluginEvent implements Cancellable
 {
     use CancellableTrait;
 
-    public function __construct()
+    public function __construct(
+        protected string $url,
+        protected ?string $oldSha = null,
+        protected ?string $newSha = null
+    )
     {
         parent::__construct(QfZidian::getInstance());
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl() : string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getOldSha() : ?string
+    {
+        return $this->oldSha;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNewSha() : ?string
+    {
+        return $this->newSha;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url) : void
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * WARNING: Changing this will not update the URL itself.
+     * @param string|null $newSha
+     */
+    public function setNewSha(?string $newSha) : void
+    {
+        $this->newSha = $newSha;
     }
 
 }
